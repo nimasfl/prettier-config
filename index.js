cc0f3b461ac66f813e11dad5c19e6bdd15de38d8
nima soufiloo <nima.soufiloo@gmail.com>
2020-08-20 21:22:12 +0430

    changed remote repo. changed trailing comma option

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-module.exports = {  arrowParens: "avoid",  printWidth: 95,  vueIndentScriptAndStyle: true,  trailingComma: "es5",  tabWidth: 2,  semi: true,  singleQuote: false,  jsxBracketSameLine: true};
+module.exports = {
+  arrowParens: "avoid",
+  printWidth: 95,
+  vueIndentScriptAndStyle: true,
+  trailingComma: "none",
+  tabWidth: 2,
+  semi: true,
+  singleQuote: false,
+  jsxBracketSameLine: true
+};
